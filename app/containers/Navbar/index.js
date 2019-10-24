ee48727bbe2534cd22b5faa7b3dbdba8c6cf5a1f
AlvinHChang <solstory@gmail.com>
2019-10-23 22:12:26 -0500

    added eslint and started fixing lint errors

--- app/containers/Navbar/index.js
+++ app/containers/Navbar/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /*
  * Navbar
  *
@@ -5,89 +6,86 @@
  *
  */
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import styles from './Navbar.css';
 import tllcLogo from '../../images/tllccLogo.png';
 import LanguageToggle from '../LanguageToggle';
-import { useWindowDimensions } from "../WindowContext";
-
-function Navbar() {
-    const { width } = useWindowDimensions();
-    return (
-        <header className={styles.navbarContainer}>
-            <nav className={styles.navbar}>
-                <div>
-                    <Link to="/home">
-                        <img className={styles.tllcLogo} alt="tllcLogo" src={tllcLogo} />
-                    </Link>
-                </div>
-                { width > 1000 ? renderDesktop() : renderMobile()}
-            </nav>
-        </header>
-    );
-}
+import { useWindowDimensions } from '../WindowContext';
 
 const renderMobile = () => {
-    const [isOpen, toggleOpen] = useState(false);
-    return (
-        <div className={styles.navbarRightContainerMobile}>
-            <button type="button" onClick={() => toggleOpen(!isOpen)}>
+  const [isOpen, toggleOpen] = useState(false);
+  return (
+    <div className={styles.navbarRightContainerMobile}>
+      <button type="button" onClick={() => toggleOpen(!isOpen)}>
                 click me
-            </button>
-            { isOpen ?
-                (
-                    <>
-                            <Link className={styles.linksFlexItemMobile} to="/home">
-                                <FormattedMessage {...messages.home} />
-                            </Link>
-                            <Link className={styles.linksFlexItemMobile} to="/about">
-                                <FormattedMessage {...messages.about} />
-                            </Link>
-                            <Link className={styles.linksFlexItemMobile} to="/sermon">
-                                <FormattedMessage {...messages.sermon} />
-                            </Link>
-                            <Link className={styles.linksFlexItemMobile} to="/gallery">
-                                <FormattedMessage {...messages.gallery} />
-                            </Link>
-                            <Link className={styles.linksNewcomer} to="/newcomer">
-                                <FormattedMessage {...messages.newcomer} />
-                            </Link>
-                            <div className={styles.linksFlexItemMobile}>
-                                <LanguageToggle />
-                            </div>
-                    </>
-                ) : null
-            }
-        </div>
-    )
-};
-const renderDesktop = () => {
-    return (
-    <div className={styles.navbarRightContainer}>
-        <Link className={styles.linksFlexItem} to="/home">
-            <FormattedMessage {...messages.home} />
-        </Link>
-        <Link className={styles.linksFlexItem} to="/about">
-            <FormattedMessage {...messages.about} />
-        </Link>
-        <Link className={styles.linksFlexItem} to="/sermon">
-            <FormattedMessage {...messages.sermon} />
-        </Link>
-        <Link className={styles.linksFlexItem} to="/gallery">
-            <FormattedMessage {...messages.gallery} />
-        </Link>
-        <Link className={styles.linksNewcomer} to="/newcomer">
-            <FormattedMessage {...messages.newcomer} />
-        </Link>
-        <div className={styles.linksFlexItem}>
-            <LanguageToggle />
-        </div>
+      </button>
+      { isOpen
+        ? (
+          <>
+            <Link className={styles.linksFlexItemMobile} to="/home">
+              <FormattedMessage {...messages.home} />
+            </Link>
+            <Link className={styles.linksFlexItemMobile} to="/about">
+              <FormattedMessage {...messages.about} />
+            </Link>
+            <Link className={styles.linksFlexItemMobile} to="/sermon">
+              <FormattedMessage {...messages.sermon} />
+            </Link>
+            <Link className={styles.linksFlexItemMobile} to="/gallery">
+              <FormattedMessage {...messages.gallery} />
+            </Link>
+            <Link className={styles.linksNewcomer} to="/newcomer">
+              <FormattedMessage {...messages.newcomer} />
+            </Link>
+            <div className={styles.linksFlexItemMobile}>
+              <LanguageToggle />
+            </div>
+          </>
+        ) : null}
     </div>
-    )
-
+  );
 };
+const renderDesktop = () => (
+  <div className={styles.navbarRightContainer}>
+    <Link className={styles.linksFlexItem} to="/home">
+      <FormattedMessage {...messages.home} />
+    </Link>
+    <Link className={styles.linksFlexItem} to="/about">
+      <FormattedMessage {...messages.about} />
+    </Link>
+    <Link className={styles.linksFlexItem} to="/sermon">
+      <FormattedMessage {...messages.sermon} />
+    </Link>
+    <Link className={styles.linksFlexItem} to="/gallery">
+      <FormattedMessage {...messages.gallery} />
+    </Link>
+    <Link className={styles.linksNewcomer} to="/newcomer">
+      <FormattedMessage {...messages.newcomer} />
+    </Link>
+    <div className={styles.linksFlexItem}>
+      <LanguageToggle />
+    </div>
+  </div>
+);
+
+function Navbar() {
+  const { width } = useWindowDimensions();
+  return (
+    <header className={styles.navbarContainer}>
+      <nav className={styles.navbar}>
+        <div>
+          <Link to="/home">
+            <img className={styles.tllcLogo} alt="tllcLogo" src={tllcLogo} />
+          </Link>
+        </div>
+        { width > 1000 ? renderDesktop() : renderMobile()}
+      </nav>
+    </header>
+  );
+}
+
 
 export default Navbar;
